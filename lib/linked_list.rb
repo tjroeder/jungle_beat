858248810e326a9b10793eced6ac5f2adbf8b3ea
Tim Roeder <timothyjroeder@gmail.com>
2022-01-01 16:19:34 -0700

    Feat: add #append and #count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,23 @@
+require_relative './node'
 class LinkedList
-  attr_reader :head
+  attr_accessor :head
 
   def initialize
     @head = nil
+  end
+
+  def append(new_node)
+    @head = Node.new(new_node)
+    new_node
+  end
+
+  def count
+    return 0 unless @head
+    count = 1
+    current = @head
+    while current.next_node != nil do
+      count += 1
+    end
+    count
   end
 end
