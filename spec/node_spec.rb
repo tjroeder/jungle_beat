5abc402d44976f5c830cbd05d40fe5483a6e55a1
Tim Roeder <timothyjroeder@gmail.com>
2022-01-01 15:31:33 -0700

    Test: data and next_node attribute tests

---
+++ spec/node_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/node'
+
+RSpec.describe Node, type: :model do
+  let!(:node_1) { Node.new('plop') }
+
+  describe 'attributes' do
+    it 'has data' do
+      expect(node_1.data).to eq('plop')
+    end
+
+    it 'has no next node originally' do
+      expect(node_1.next_node).to eq(nil)
+    end
+  end
+end
