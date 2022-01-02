055f6930bbab00c09b9d4889d9bbab8ef519b78f
Tim Roeder <timothyjroeder@gmail.com>
2022-01-01 17:38:03 -0700

    Test: update test for #next_node attr_accessor

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Node, type: :model do
     it 'has no next node originally' do
       expect(node_1.next_node).to eq(nil)
     end
+
+    it 'can update next_node' do
+      node_2 = Node.new('doop')
+      node_1.next_node = node_2
+
+      expect(node_1.next_node).to eq(node_2)
+    end
+  
   end
 end
